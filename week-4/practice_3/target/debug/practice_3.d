db34c8f696f6b545b505a0afcc7fe76dd37f7eda
Fxhdrtw <fahd.animashaun@pau.edu.ng>
2025-10-22 12:54:50 +0100

    Delete week-4 directory

--- week-4/practice_3/target/debug/practice_3.d
+++
@@ -1,1 +0,0 @@
-C:\Users\oyinl\f.animashaunCOS101\week-4\practice_3\target\debug\practice_3.exe: C:\Users\oyinl\f.animashaunCOS101\week-4\practice_3\src\main.rs
--- week-4/practice_8/target/debug/practice_8.d
+++
@@ -1,1 +0,0 @@
-C:\Users\oyinl\f.animashaunCOS101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\oyinl\f.animashaunCOS101\week-4\practice_8\src\main.rs
