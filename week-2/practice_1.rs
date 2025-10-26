1258c14f9e92e7d3e680bccdcdf723c903676557
Fahd Oyinlola Animashaun <fahd.animashaun@pau.edu.ng>
2025-10-26 14:20:43 +0100

    My second commit

---
+++ week-2/practice_1.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Welcome to COS 191!");
+    println!("\nThe course learning outcome is as follows:");
+    println!("\n1. Distinguish between computer and computer programming,\n
+    2. Develop some techniques in computer science,\n
+    3. Understand the different areas of study in computer science,\n
+    4. Navigate the career prospects in computer science,\n
+    5. Conversant with computer programming concepts.");
+}
---
+++ week-2/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+
+    println!(); // prints just a blank line
+
+    println!("hello "); // prints "hello"
+
+    println!("format {} arguments", "some"); // prints formatted text
+
+    println!("My name is {}. I am the class rep of stream 1", "Dagogo William-Jumbo");
+}
